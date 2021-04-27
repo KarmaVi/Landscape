fbf69a7de78461d266fedf9e3cba911ac266977e
KarmaVi <karma9513@gmail.com>
2021-04-27 21:49:29 +0200

    problem with colors

---
+++ Assets/Scripts/MainGui.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGui : MonoBehaviour
+{
+    [SerializeField] private GUISkin _skin;
+    
+    private Rect windowRect = new Rect (Screen.width * 0.4f, Screen.height * 0.4f, 330, 300);
+
+    public void OnGUI()
+    {
+        windowRect = GUI.Window(0, windowRect, WindowFunction, "Pause"); 
+    }
+    public void WindowFunction(int windowID)
+    {
+        if (GUI.Button(new Rect(windowRect.width * 0.2f, 30, 220, 50), "Continue"))
+            print("Continue");
+        if (GUI.Button(new Rect(windowRect.width * 0.2f, 90, 220, 50), "Restart"))
+            print("Restart");
+        if (GUI.Button(new Rect(windowRect.width * 0.2f, 150, 220, 50), "Settings"))
+            print("Settings");
+        if (GUI.Button(new Rect(windowRect.width * 0.2f, 210, 220, 50), "Exit"))
+            print("Exit");
+    }
+}
