706c5e44b761004c34b151a14a0db689d599061e
KarmaVi <karma9513@gmail.com>
2021-05-12 22:04:31 +0200

    1

---
+++ Assets/Scripts/Player/WarriorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorController : MonoBehaviour
+{
+    [SerializeField] [Range(0, 5)]
+    private float _speed;
+
+    [SerializeField] [Range(-1, 1)]
+    private float _direction;
+
+    [SerializeField]
+    private Animator _animator;
+
+    protected void Update()
+    {
+        _animator.SetFloat("Speed", _speed);
+        _animator.SetFloat("Direction", _direction);
+    }
+    public void RunEvent(int velue)
+    {
+        Debug.Log("RunEvent");
+    }
+
+}
